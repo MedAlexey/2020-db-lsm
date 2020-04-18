34cbb8ac8ffbd19a5de4b471deb0e30182bb4b5c
Alexey <medvedev.lasta@yandex.ru>
2020-04-18 20:33:17 +0700

    DAO implemented

---
+++ src/main/java/ru/mail/polis/medvedev/DAOImpl.java
@@ -0,0 +1,40 @@
+package ru.mail.polis.medvedev;
+
+import org.jetbrains.annotations.NotNull;
+import ru.mail.polis.DAO;
+import ru.mail.polis.Record;
+
+import java.nio.ByteBuffer;
+import java.util.Iterator;
+import java.util.SortedMap;
+import java.util.TreeMap;
+
+public class DAOImpl implements DAO {
+
+    private final SortedMap<ByteBuffer, ByteBuffer> map = new TreeMap<>();
+
+    @NotNull
+    @Override
+    public Iterator<Record> iterator(@NotNull final ByteBuffer from) {
+        return map.tailMap(from)
+                .entrySet()
+                .stream()
+                .map(element -> Record.of(element.getKey(), element.getValue()))
+                .iterator();
+    }
+
+    @Override
+    public void upsert(@NotNull final ByteBuffer key, @NotNull final ByteBuffer value) {
+        map.put(key, value);
+    }
+
+    @Override
+    public void remove(@NotNull final ByteBuffer key) {
+        map.remove(key);
+    }
+
+    @Override
+    public void close() {
+        map.clear();
+    }
+}
